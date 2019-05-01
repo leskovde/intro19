1326aa3d2b26ff3c005e1cf89c7059a351c746ce
leskovde <47887202+leskovde@users.noreply.github.com>
2019-05-02 01:47:36 +0200

    Update ansi-to-kr.sh

--- 09/ansi-to-kr.sh
+++ 09/ansi-to-kr.sh
@@ -12,7 +12,7 @@ echo -n > recur_temp.txt
 count=1
 for file in "$@"; do
         echo "file" | grep '.[ch]' > /dev/null
-        if [ $? -e 0 ]; then
+        if [ $? -eq 0 ]; then
                 ls -1 $file/*.{c,h}  >> recur_temp.txt
         fi
         while read -r line; do
@@ -44,13 +44,15 @@ for file in "$@"; do
 done
 IFS=$'\n'
 for file in $(cat recur_temp.txt); do
-        echo "$file"
         while read -r line; do
                 # recursion
                 a=$(echo "$line" | sed -n '/^ *#/ p');
                 grep "$a" recur_temp.txt > /dev/null
                 if [ $? -ne 0 ]; then
-                        echo "$a" >> recur_temp.txt
+                        filePath=$(readlink -f $file)
+                        path=$(echo "$line" | sed 's/#include//' | sed 's/[<]/\/usr\/include\//' |
+                        sed 's/[>]//' | sed "s_\"_$filePath/_" | sed 's/[\"]//' | sed 's/^ *//')
+                        echo "$path" >> recur_temp.txt
                 fi
                 # declaration
                 echo "$line" | sed -n '/^\ *\/\// p' >> $file-temp$count
